a4f76a58aa1470f9568470d7000e0de560f9a81d
alamin <alamin@gmail.com>
2021-08-23 00:42:29 +0600

    chart with class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ApexCharts from "./ApexChart";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+      <ApexCharts />
     </div>
   );
 }
